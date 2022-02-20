dd456c5ba1da103c02887f0e7470314531d47bdb
mcqueen <chqueen.tw@gmail.com>
2022-02-20 12:53:22 +0800

    init

--- App.js
+++ App.js
@@ -4,9 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>110919002藍中崑</Text>
+      <Text>asiodjasdijasildj</Text>
       <StatusBar style="auto" />
     </View>
+    
+  
+    
   );
 }
 
