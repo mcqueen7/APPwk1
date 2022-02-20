0d7ce2166107bdda71fe97173294488623c04664
mcqueen <chqueen.tw@gmail.com>
2022-02-20 18:00:12 +0800

    first commit

--- App.js
+++ App.js
@@ -3,22 +3,39 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>110919002藍中崑</Text>
-      <Text>asiodjasdijasildj</Text>
+    <View style={styles.container1}>
+      
+      <View style={styles.container2}>
+      <Text style={styles.text}>110919002藍中崑</Text>
+      </View>
+      <View style={styles.container2}>
+      <Text style={styles.text}>Hellow RN</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
-    
-  
-    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container1: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    backgroundColor: 'rgb(20,150,255)',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container2: {
+    flex: 1,
+    height: 200,
+    margin: 20,
+    backgroundColor: 'rgb(0,255,255)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    flexWrap: 'wrap',
+    width: 130,
+    textAlign: 'center',
   },
 });
